7c8e68526b45c8370789c69b50e4360c8d8a1dc8
SULTAN <ramhziworld@gmail.com>
2021-06-16 19:54:51 +0100

    Added error color

--- lib/utils/assets_colors.dart
+++ lib/utils/assets_colors.dart
@@ -5,4 +5,5 @@ class AssetColors {
   static const Color titleTextColor = Color(0xFF2B2B2B);
   static const Color subTextColor = Color(0xFF878787);
   static const Color borderColor = Color(0xFFF5F6F7);
+  static const Color errColor = Color(0xFFDF2153);
 }
