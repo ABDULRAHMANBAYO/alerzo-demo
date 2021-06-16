36479cc421240bfb6aebe7d9f150b6bcb7492f71
SULTAN <ramhziworld@gmail.com>
2021-06-16 23:13:51 +0100

    Added login and sign up text style

---
+++ lib/styles/login.dart
@@ -0,0 +1,48 @@
+import 'package:alerzo_demo/utils/assets_colors.dart';
+import 'package:flutter/material.dart';
+
+loginTextStyle(width) => TextStyle(
+    fontSize: width * 0.07,
+    color: AssetColors.titleTextColor,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700);
+welcomeTextStyle(width) => TextStyle(
+    fontSize: width * 0.05,
+    color: AssetColors.subTextColor,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400);
+textFieldPlaceholderTextStyle(width) => TextStyle(
+    fontSize: width * 0.035,
+    color: AssetColors.subTextColor,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400);
+labelTextFieldTextStyle(width) => TextStyle(
+    fontSize: width * 0.05,
+    color: AssetColors.primaryColor,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400);
+errorTextStyle(width) => TextStyle(
+    fontSize: width * 0.05,
+    color: AssetColors.errColor,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400);
+textFieldTextStyle(width) => TextStyle(
+    fontSize: width * 0.03,
+    color: AssetColors.subTextColor,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400);
+buttonTextStyle(width) => TextStyle(
+    fontSize: width * 0.045,
+    color: Colors.white,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700);
+bottomTextOneStyle(width) => TextStyle(
+    fontSize: width * 0.045,
+    color: AssetColors.subTextColor,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w400);
+bottomTextTwoStyle(width) => TextStyle(
+    fontSize: width * 0.045,
+    color: AssetColors.primaryColor,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w700);
