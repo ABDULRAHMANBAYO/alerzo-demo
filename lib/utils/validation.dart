fd56e267267e170064ea9ba1e906cf63fafee627
SULTAN <ramhziworld@gmail.com>
2021-06-16 22:47:40 +0100

    Added validation

---
+++ lib/utils/validation.dart
@@ -0,0 +1,33 @@
+import 'string_resources.dart';
+
+String? validateEmail(value) {
+  var pattern = r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$';
+  RegExp regex = new RegExp(pattern);
+  if (value!.isEmpty) {
+    return StringResources.EMAIL_IS_EMPTY;
+  } else if (!regex.hasMatch(value)) {
+    return StringResources.ENTER_VALID_EMAIL_REQUIRED;
+  } else
+    return null;
+}
+
+String? validatePassword(value) {
+  var pattern =
+      // r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*(),.?":{}|<>\~]).{8,}$';
+  RegExp regex = new RegExp(pattern);
+  print(value);
+  if (value.isEmpty) {
+    return StringResources.PASSWORD_REQUIRED;
+  } else if (!value.contains(RegExp(r'[A-Z]'))) {
+    return StringResources.UPPER_CASE_REQUIRED;
+  } else if (!value.contains(RegExp(r'[a-z]'))) {
+    return StringResources.LOWER_CASE_REQUIRED;
+  } else if (!value.contains(RegExp(r'[0-9]'))) {
+    return StringResources.NUMBER_REQUIRED;
+  } else if (!value.contains(RegExp(r'[!@#$%^&*(),.?":{}|<>\~]'))) {
+    return StringResources.SPECIAL_CHARACTER_REQUIRED;
+  } else if (!regex.hasMatch(value)) {
+    return StringResources.ENTER_VALID_PASSWORD_REQUIRED;
+  }
+}
