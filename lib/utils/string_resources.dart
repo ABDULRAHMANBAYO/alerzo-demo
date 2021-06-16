972c32b0a2f6a86a6b573bfecc0075ef76bd6634
SULTAN <ramhziworld@gmail.com>
2021-06-16 19:43:26 +0100

    Added string resources

---
+++ lib/utils/string_resources.dart
@@ -0,0 +1,18 @@
+class StringResources {
+  static const LET_SIGN_YOU_IN = "Let’s Sign You In";
+  static const WELCOME_BACK = "Welcome back, You’ve been missed!";
+  static const EMAIL_ADDRESS = "Email Address";
+  static const PASSWORD = "Password";
+  static const LOGIN = "Log In";
+  static const DONT_HAVE_AN_ACCOUNT = "Don’t have an account? ";
+  static const SIGN_UP = "Sign Up";
+  static const CREATE_AN_ACCOUNT = "Create an Account";
+  static const JOIN_US = "Join us, We’re happy to have you! ";
+  static const FIRST_NAME = "First Name";
+  static const LAST_NAME = "Last Name";
+  static const WELCOME_NAME = "Hi Ade";
+  static const INFORMATION = "The information below is API based";
+  static const ID = "ID:";
+  static const TITLE = "Title:";
+  static const BODY = "Body:";
+}
