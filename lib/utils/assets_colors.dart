6da41bfffc7a461fdce72560dd34a484d43f2f35
SULTAN <ramhziworld@gmail.com>
2021-06-16 19:52:54 +0100

    Added color assets

---
+++ lib/utils/assets_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AssetColors {
+  static const Color primaryColor = Color(0xFF4D14DE);
+  static const Color titleTextColor = Color(0xFF2B2B2B);
+  static const Color subTextColor = Color(0xFF878787);
+  static const Color borderColor = Color(0xFFF5F6F7);
+}
