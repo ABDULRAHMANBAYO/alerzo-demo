92d862b8964ad9556badd936ae2315a6613c5f60
SULTAN <ramhziworld@gmail.com>
2021-06-16 23:13:35 +0100

    Added border

--- lib/utils/borders.dart
+++ lib/utils/borders.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 import 'assets_colors.dart';
 
 InputBorder focused = OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(2)),
+    borderRadius: BorderRadius.all(Radius.circular(10)),
     borderSide: BorderSide(width: 1, color: AssetColors.primaryColor));
 InputBorder disabled = OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(2)),
+    borderRadius: BorderRadius.all(Radius.circular(10)),
     borderSide: BorderSide(width: 1, color: AssetColors.borderColor));
 InputBorder enabled = OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(2)),
-    borderSide: BorderSide(width: 1, color: AssetColors.primaryColor));
+    borderRadius: BorderRadius.all(Radius.circular(10)),
+    borderSide: BorderSide(width: 1, color: AssetColors.borderColor));
 InputBorder error = OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(2)),
+    borderRadius: BorderRadius.all(Radius.circular(10)),
     borderSide: BorderSide(width: 1, color: AssetColors.errColor));
 InputBorder focusedError = OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(2)),
+    borderRadius: BorderRadius.all(Radius.circular(10)),
     borderSide: BorderSide(width: 1, color: AssetColors.errColor));
