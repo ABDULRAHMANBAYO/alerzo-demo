5672ed0ead0f96b8c56cc902f1ecb9fbb8e51740
SULTAN <ramhziworld@gmail.com>
2021-06-17 16:22:57 +0100

    Added image asset

---
+++ lib/utils/assets_images.dart
@@ -0,0 +1,5 @@
+class AssetResources {
+  static const LOGIN_IMG = "assets/images/global/login.svg";
+  static const LOGIN_PNG = "assets/images/global/login_one.png";
+  static const HAND_EMOJI = "assets/images/global/emoji.svg";
+}
