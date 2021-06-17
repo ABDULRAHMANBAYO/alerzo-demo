c0c716f7d17db0a29935c435619117035a7c2c53
SULTAN <ramhziworld@gmail.com>
2021-06-17 16:23:25 +0100

    Updated string res

--- lib/utils/string_resources.dart
+++ lib/utils/string_resources.dart
@@ -7,7 +7,7 @@ class StringResources {
   static const DONT_HAVE_AN_ACCOUNT = "Don’t have an account? ";
   static const HAVE_AN_ACCOUNT = "Have an account? ";
   static const SIGN_UP = "Sign Up";
-  static const CREATE_AN_ACCOUNT = "Create an Account";
+  static const CREATE_AN_ACCOUNT = "Create An Account";
   static const JOIN_US = "Join us, We’re happy to have you! ";
   static const FIRST_NAME = "First Name";
   static const LAST_NAME = "Last Name";
