3d53a9ffb4ff1f1b2edc237283155c37bece2c0d
SULTAN <ramhziworld@gmail.com>
2021-06-17 09:39:47 +0100

    Changed post request to get

--- lib/services/http_request.dart
+++ lib/services/http_request.dart
@@ -1,14 +1,10 @@
 import 'package:http/http.dart' as httpClient;
 
 class HttpService {
-  static Future post(String location,
-      {dynamic bodyData, Map<String, String>? headers}) async {
+  static Future get(String location, {Map<String, String>? headers}) async {
     var url = Uri.parse(location);
-    Map<String, String> _headers = Map<String, String>();
-    _headers.putIfAbsent('Content-Type', () => 'application/json');
-    if (headers != null) _headers.addAll(headers);
     try {
-      return httpClient.post(url, headers: _headers, body: bodyData);
+      return httpClient.get(url, headers: headers);
     } catch (e) {
       return e;
     }
